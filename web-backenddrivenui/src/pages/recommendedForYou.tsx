37a0e8e77a9834584d21c1fa11e881478a3dc6b7
Angela Isabel Carrera <aicarrera@mondragon.edu>
2023-03-13 15:36:40 +0100

    responsiveness

--- web-backenddrivenui/src/pages/recommendedForYou.tsx
+++ web-backenddrivenui/src/pages/recommendedForYou.tsx
@@ -1,4 +1,4 @@
-import { Heading, Icon, VStack, HStack } from "@chakra-ui/react"
+import { Heading, Icon, VStack, HStack, Stack } from "@chakra-ui/react"
 import { BiBookmarkHeart } from "react-icons/bi"
 import * as constants from "../config/constants";
 import ServicesRecommended from "./servicesRecommended";
@@ -7,9 +7,9 @@ const Recommended = ({firstRecommended, setActiveTab, setServiceSelected}) => {
   return(
   <VStack w="full" h="full" p={10} alignItems="flex-start"  spacing={'2px'} borderWidth="1px" borderColor="gray.300" borderRadius="md">
     <Heading as='h4' size='md'>  <Icon as={BiBookmarkHeart} />  Recommended for you </Heading>         
-    <HStack w="full" h="full" p={10} alignItems="flex-start"  spacing={'5px'}>  
+    <Stack  direction={['column', 'row']}  w="full" h="full" p={10} alignItems="flex-start"  spacing={'5px'}>  
     <ServicesRecommended recommended={firstRecommended}  includeControls={true}  setActiveTab={setActiveTab} setServicePreparation={setServiceSelected} />
-    </HStack>
+    </Stack>
   </VStack>
   );
 
